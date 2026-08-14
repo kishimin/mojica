c81a80779f07480e5d1b68e2027a97f7e9241157
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-08-14 10:18:19 +0900

    test: require successful results to contain data
    Prevent the success factory from representing an outcome without generated image data.

---
+++ backend/Mojica.Api.Tests/Ports/ImageGenerationPortResultSmallTests.cs
@@ -0,0 +1,13 @@
+using Mojica.Api.Ports;
+
+namespace Mojica.Api.Tests.Ports;
+
+public sealed class ImageGenerationPortResultSmallTests
+{
+    [Fact]
+    public void ImageGenerationPortResult_Success_WhenDataIsNull_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            ImageGenerationPortResult.Success(null!));
+    }
+}
